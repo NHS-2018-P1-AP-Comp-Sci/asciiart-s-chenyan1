9a5e82581ce4fc8fd3bdd8947acd3d5183b0fbc8
s-chenyan1 <s-chenyan1@53S50030924.it.bsd405.org>
2018-09-20 08:12:35 -0700

    Turning in Project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,4 +14,65 @@
  *
  */
 
+
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+	public static void main(String[] args) {
+		boyComputer();
+		girlComputer();
+	}
+	
+	public static void girlComputer() {
+		girlHead();
+		face();
+		girlNeck();
+		body();
+	}
+	
+	public static void boyComputer() {
+		boyHead();
+		face();
+		boyNeck();
+		body();
+	}
+	
+	public static void boyHead() {
+		System.out.println("   _______________");
+		System.out.println("  | _____________ |");
+	}
+	
+	public static void boyNeck() {
+		System.out.println("  | |___     ___| |");
+		System.out.println("  |_____|\\_/|_____|");
+		System.out.println("    _|__|/ \\|__|_");
+	}
+	
+	public static void girlHead() {
+		System.out.println("    |*\\_/*|_______");
+		System.out.println("  | |_/-\\_|______ |");
+	}
+	
+	public static void girlNeck() {
+		System.out.println("  | |___________| |");
+		System.out.println("  |_______________|");
+		System.out.println("     _|_______|_");
+	}
+	
+	public static void face() {
+		System.out.println("  | |           | |");
+		System.out.println("  | |   O   O   | |");
+		System.out.println("  | |     _     | |");
+		System.out.println("  | |   \\___/   | |");
+	}
+	
+	public static void body() {
+		System.out.println("   / *********** \\");
+		System.out.println(" /  *************  \\");
+		System.out.println("---------------------");
+	}
+	
+}
+
+
+
+
